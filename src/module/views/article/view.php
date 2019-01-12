3d59142f5e8434a127e6098a8d121500451531be
Vitaliy <theyamshikov@yandex.ru>
2019-01-13 01:05:12 +0600

    refacto

--- src/module/views/article/view.php
+++ src/module/views/article/view.php
@@ -6,11 +6,10 @@ use yii2lab\extension\web\helpers\Page;
 use yii2lab\extension\yii\helpers\Html;
 use yii2module\guide\domain\entities\ProjectEntity;
 use yii2module\guide\module\helpers\NavigationHelper;
-use yii2module\markdown\widgets\helpers\ArticleMenuHelper;
-use yii2module\markdown\widgets\helpers\MarkdownHelper;
-use yii2module\markdown\widgets\Markdown;
+use yii2lab\extension\markdown\widgets\Markdown;
 use yii2module\guide\domain\enums\GuidePermissionEnum;
-
+use yii2lab\extension\markdown\widgets\helpers\ArticleMenuHelper;
+use yii2lab\extension\markdown\widgets\helpers\MarkdownHelper;
 
 $this->title = $entity->title;
 
